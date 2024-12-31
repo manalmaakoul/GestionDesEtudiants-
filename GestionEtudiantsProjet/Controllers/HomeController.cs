edd8f2a1ab1674777de402eb281c8551c3c8c5c3
manalmaakoul <manalmaakoul22@gmail.com>
2024-12-31 19:09:40 +0100

    ajout du dockerfile

--- GestionEtudiantsProjet/Controllers/HomeController.cs
+++ GestionEtudiantsProjet/Controllers/HomeController.cs
@@ -17,6 +17,10 @@ namespace GestionEtudiantsProjet.Controllers
         {
             return View();
         }
+        public IActionResult CRMEF()
+        {
+            return View();
+        }
 
         public IActionResult Privacy()
         {
--- GestionEtudiantsProjet/Program.cs
+++ GestionEtudiantsProjet/Program.cs
@@ -18,12 +18,12 @@ builder.Services.AddScoped<ICoursEnseignantService, CoursEnseignantService>();
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
-if (!app.Environment.IsDevelopment())
-{
-    app.UseExceptionHandler("/Home/Error");
-    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
-    app.UseHsts();
-}
+//if (!app.Environment.IsDevelopment())
+//{
+//    app.UseExceptionHandler("/Home/Error");
+//    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+//    app.UseHsts();
+//}
 
 app.UseHttpsRedirection();
 app.UseStaticFiles();
@@ -34,6 +34,6 @@ app.UseAuthorization();
 
 app.MapControllerRoute(
     name: "default",
-    pattern: "{controller=Home}/{action=Index}/{id?}");
+    pattern: "{controller=Home}/{action=CRMEF}/{id?}");
 
 app.Run();
